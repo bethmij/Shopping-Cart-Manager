5ceb0801811ce89842df0d99a23b7c5863f8f677
Dinarbrooke <bethmij@gmail.com>
2024-01-04 10:44:25 +0530

    Electronics added

--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -1,2 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCart {
+
+    private List productList;
+
+    public ShoppingCart() {
+    }
+
+    public ShoppingCart(List productList) {
+        this.productList = productList;
+    }
+
+    public List getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List productList) {
+        this.productList = productList;
+    }
+
+    public void addItems(){}
+
+    public void removeItems(){}
+
+    public void calcTotalCost(){}
+
+
 }
