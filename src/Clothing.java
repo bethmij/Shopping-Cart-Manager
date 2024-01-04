09844c4abaf34f8de185866d055d3cc6668b7d17
Dinarbrooke <bethmij@gmail.com>
2024-01-04 10:18:24 +0530

    Clothing updated

--- src/Clothing.java
+++ src/Clothing.java
@@ -1,2 +1,29 @@
-public class Clothing {
+public class Clothing extends Product{
+
+    private double size;
+    private String color;
+
+    public Clothing() {
+    }
+
+    public Clothing(double size, String color) {
+        this.size = size;
+        this.color = color;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
